210bd1e67d3098de64eb1f6b2c54e986799ba2dc
Karol Chaparro <karolgomez171@gmail.com>
2022-07-21 17:54:57 -0500

    listado organizado

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
-import './Components/Hearder'; 
-import './Components/Formulario';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Formulario from './Components/Formulario';
 import Header from './Components/Hearder';
-
+import TareaEstatic from './Components/TareasEstatic';
 
 
 function App() {
   return (
     <div>
       
-   <Header Titulo={"Mi aplicacion"}/>
-
-    <Formulario></Formulario>
+   <Header Titulo={"Mi lista de tareas"}/>
+   <Formulario></Formulario>
+   <TareaEstatic></TareaEstatic>
+   
      
     </div>
   );
--- src/Components/formulario.js
+++ src/Components/formulario.js
@@ -1,45 +1,22 @@
-import React, { UseState, UseEffect } from "react";
+import React, {useState} from "react";
 
 const Formulario = (promp) => {
-   const [InputText, SetInputText] = UseState("");
 
-   const manejartarea = (Event) => {
+    
+    const [InputText, SetInputText] = useState("");
+
+   const ManejarTarea = (Event) => {
     SetInputText(Event.target.velue);
-    console.log (InputText)
-   }
-
+    console.log (InputText) }
 
     return (    
         <div>
-            <span> Añadir tarea </span>
+            
             <form className="tarea">
-                <span> Añadir tarea 1° </span>
-                <input placeholder=" Ingresa aqui la tarea" type="checkbox">   </input> 
-                 <br></br>
-                <span> Añadir tarea 2° </span>
-                <input placeholder=" Ingresa aqui la tarea" type="checkbox">   </input>
-                <br></br>
-                <span> Añadir tarea 3° </span>
-                <input placeholder=" Ingresa aqui la tarea" type="checkbox">   </input>
-                <br></br>
-                <span> Añadir tarea 4° </span>
-                <input placeholder=" Ingresa aqui la tarea" type="checkbox">   </input>
-                <br></br>
-                <span> Añadir tarea 5° </span>
-                <input placeholder=" Ingresa aqui la tarea" type="checkbox">   </input>
-                <br></br>
-                <span> Añadir tarea 6°</span>
-                <input placeholder=" Ingresa aqui la tarea" type="checkbox">   </input>
-                <br></br>
-                <span> Añadir tarea 7° </span>
-                <input placeholder=" Ingresa aqui la tarea" type="checkbox">   </input>
-                <br></br>
-                <span> Añadir tarea 8° </span>
-                <input placeholder=" Ingresa aqui la tarea" type="checkbox">   </input>
-
+                <span> Añade tarea: </span>
+                <input placeholder = {"Agrega nueva tarea"}  value ={InputText} onChange={ManejarTarea} ></input>
             </form>
-            <input> </input>
-            <button></button>
+            <button>Agregar a lista</button>
         </div> 
     )
 }
